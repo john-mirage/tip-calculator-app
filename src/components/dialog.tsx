73605e2b5d8b270898ba2aa56bc14525bf679a2d
chaosdynamix <chaosdynamix@posteo.net>
2022-03-02 17:54:53 +0100

    ref: dialog padding

--- src/components/dialog.tsx
+++ src/components/dialog.tsx
@@ -3,7 +3,7 @@ import Result from '@components/result'
 
 function Dialog() {
     return (
-        <main className="w-full min-h-dialog bg-white rounded-t-3xl p-6">
+        <main className="w-full min-h-dialog bg-white rounded-t-3xl px-6 pt-6 pb-12">
             <Form />
             <Result />
         </main>
