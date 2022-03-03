d453cb77b9d14483e64b0b3aa7bc6c48d9ae8366
chaosdynamix <chaosdynamix@posteo.net>
2022-03-03 14:13:41 +0100

    feat(vite): add contexts resolve alias

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
     alias: {
       '@components': path.resolve(__dirname, 'src/components'),
       '@assets': path.resolve(__dirname, 'src/assets'),
+      '@contexts': path.resolve(__dirname, 'src/contexts'),
     },
   },
 })
